d2673cd9a42ac23d11d05c30444ec3c03df1922f
masnth <masker0801@gmail.com>
2017-01-26 15:01:28 -0800

    new file for testing

--- Lab2/muxControl.v
+++ Lab2/muxControl.v
@@ -1,16 +1,26 @@
-module muxControl (input [31:0] reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7,
-		   reg8, reg9, reg10, reg11, reg12, reg13 reg14, reg15,
+//`include "mux32to1.v"
+
+module muxControl (reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7,
+		   reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15,
 		   reg16, reg17, reg18, reg19, reg20, reg21, reg22, reg23,
-		   reg24, reg25, reg26, reg27, reg28, reg29, reg30, reg31, output reg [31:0] readData, input [4:0] sel);
+		   reg24, reg25, reg26, reg27, reg28, reg29, reg30, reg31, readData, sel);
 
-
- genvar i;
+	input [31:0] reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7,
+		   reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15,
+		   reg16, reg17, reg18, reg19, reg20, reg21, reg22, reg23,
+		   reg24, reg25, reg26, reg27, reg28, reg29, reg30, reg31;
+	
+	input[4:0] sel;
+	output [31:0] readData;
+		   
+	genvar i;
  
  generate
-	 for (i = 0; i < 32; i = i + 1) begin : eachDff
-		mux32to1 mux (.data({reg1[i], reg2[i], reg3[i], reg4[i], reg5[i], reg6[i], reg7[i], reg8[i], reg9[i], 
+	 for (i = 0; i < 32; i = i + 1) begin : eachmux
+		mux32to1 mux (.data({reg0[i],reg1[i], reg2[i], reg3[i], reg4[i], reg5[i], reg6[i], reg7[i], reg8[i], reg9[i], 
 		reg10[i], reg11[i], reg12[i], reg13[i], reg14[i], reg15[i], reg16[i], reg17[i], reg18[i],reg19[i],
 		reg20[i], reg21[i], reg22[i], reg23[i], reg24[i], reg25[i], reg26[i], reg27[i],reg28[i], reg29[i], 
 		reg30[i], reg31[i]}), .q(readData[i]), .sel(sel));
 	end
  endgenerate
+endmodule
--- Lab2/register.v
+++ Lab2/register.v
@@ -1,5 +1,5 @@
-module register(dataOut,  dataIn, clk, rst);
-	input clk, rst;
+module register(dataOut,  dataIn, enable, clk, rst);
+	input clk, rst, enable;
 	input [31:0] dataIn;
 	output wire [31:0] dataOut;
 	wire [31:0] dataOutBar;
@@ -7,26 +7,26 @@ module register(dataOut,  dataIn, clk, rst);
 	genvar i;
 	
 	generate 
-		for (i=0; i<32; i=i+1) begin: flipflop
-			DFF D (dataOut[i], dataOutBar[i], dataIn[i], clk, rst);
+		for (i=0; i<32; i=i+1) begin: eachDFF
+			DFF_e D (.Q(dataOut[i]), .Qbar(dataOutBar[i]), .dataIn(dataIn[i]), .enable(enable), .clk(clk), .rst(rst));
 		end
 	endgenerate
 
 endmodule
 
-module DFF(Q, Qbar, dataIn, clk, rst);
-	input clk, rst;
+module DFF_e(Q, Qbar, dataIn, enable, clk, rst);
+	input clk, rst, enable;
 	input dataIn;
 	output reg Q;
 	output reg Qbar;
-	reg ps, ns;
+
 	always @ (posedge clk or negedge rst)
-		if (~rst) begin
-			Q = 1'b0;
-			Qbar = 1'b1;
+		if (!rst) begin
+			Q <= 1'b0;
+			Qbar <= 1'b1;
 		end
-		else begin 
-			Q = dataIn;
-			Qbar = ~dataIn;
+		else if(enable) begin 
+			Q <= dataIn;
+			Qbar <= ~dataIn;
 		end
 endmodule 
