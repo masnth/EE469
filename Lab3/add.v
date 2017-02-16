32050d7900eb6595256318c69abe23f7348de4ca
baotantran <baotantran92@gmail.com>
2017-02-16 12:42:06 -0800

    Update add.v

--- Lab3/add.v
+++ Lab3/add.v
@@ -22,3 +22,34 @@ module add(data1, data2, result, v, c, clk);
 	end
 endmodule
 	
+module add_testbench();
+
+	reg clk;
+	reg [31:0] data1, data2;
+	wire [31:0] result;
+	wire v, c;
+	wire [32:0] temp;
+	
+	add dux (data1, data2, result, v, c, clk, temp);
+	
+	parameter CLOCK_PERIOD=100;
+	initial begin
+	clk <= 0;
+	forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	initial begin 
+	
+	 data1 = 15; data2 = 585676;      @(posedge clk);
+	 data1 = 3547; data2 = 4896;		@(posedge clk);
+	 data1 = 56; data2 = 456;  @(posedge clk);
+	 data1 = 15; data2 = 45466;  @(posedge clk);
+	 data1 = 489; data2 = 89768;  @(posedge clk);
+	 data1 = 190; data2 = 5686894;  @(posedge clk);
+	 data1 = 2147483647; data2 = 10;  @(posedge clk);
+	                                  @(posedge clk);
+	
+	$stop();
+	
+	end
+endmodule 
