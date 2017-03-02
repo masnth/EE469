08aea56b2fcfc28982cad410f0efa1adf0218a09
baotantran <baotantran92@gmail.com>
2017-03-02 02:51:39 -0800

    Update instr_dec.v

--- Lab4/instr_dec.v
+++ Lab4/instr_dec.v
@@ -1,13 +1,28 @@
-module instr_decoder();
-	input
-	output
+module instr_decoder(instruction, imm_sig, rn, rd, rm, imm_value, enLoad, enStore, clk);
 	
+	input clk;
+	input [31:0] instruction;
+	output reg [4:0] rn, rd, rm;
+	output reg [11:0] imm_value;
+	output reg enLoad;
+	output reg enStore;
+	output reg imm_sig;
+	output reg [2:0] aluControl;
+	
+	parameter [2:0] op_nop = 3'b000; op_add = 3'b001; op_sub = 3'b010; op_and = 3'b011;
+op_or = 3'b100; op_eor = 3'b101; op_lsl = 3'b110;	
+	
+always@(*) begin 
 	case (instruction[31:21])
 	// ADDI 
 	1001000100x: begin
 		imm_value = instruction[21:10];
 		rn = instruction[9:5];	// second operand
 		rd = instruction[4:0];	// destination
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b1;
+		aluControl = op_add;
 		end
 	
 	//SUBI
@@ -15,6 +30,10 @@ module instr_decoder();
 		imm_value = instruction[21:10];
 		rn = instruction[9:5];	// second operand
 		rd = instruction[4:0];	// destination
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b1;
+		aluControl = op_sub;
 		end
 		
 	//ANDI
@@ -22,6 +41,10 @@ module instr_decoder();
 		imm_value = instruction[21:10];
 		rn = instruction[9:5];	// second operand
 		rd = instruction[4:0];	// destination
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b1;
+		aluControl = op_and;
 		end
 		
 	//ORRI
@@ -29,6 +52,10 @@ module instr_decoder();
 		imm_value = instruction[21:10];
 		rn = instruction[9:5];	// second operand
 		rd = instruction[4:0];	// destination
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b1;
+		aluControl = op_or;
 		end
 		
 	//EORI
@@ -36,6 +63,10 @@ module instr_decoder();
 		imm_value = instruction[21:10];
 		rn = instruction[9:5];	// second operand
 		rd = instruction[4:0];	// destination
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b1;
+		aluControl = op_eor;
 		end
 		
 	//ADD
@@ -43,6 +74,10 @@ module instr_decoder();
 		rm = instruction[20:16];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b1;
+		aluControl = op_add;
 		end
 		
 	//SUB
@@ -50,6 +85,10 @@ module instr_decoder();
 		rm = instruction[20:16];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b0;
+		aluControl = op_sub;
 		end
 		
 	//AND
@@ -57,6 +96,10 @@ module instr_decoder();
 		rm = instruction[20:16];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b0;
+		aluControl = op_and;
 		end
 	
 	//ORR
@@ -64,6 +107,10 @@ module instr_decoder();
 		rm = instruction[20:16];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b0;
+		aluControl = op_or;
 		end
 	
 	//EOR
@@ -71,6 +118,10 @@ module instr_decoder();
 		rm = instruction[20:16];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b0;
+		aluControl = op_eor;
 		end
 	
 	//LSL
@@ -79,6 +130,10 @@ module instr_decoder();
 		rn = instruction[9:5];
 		rd = instruction[4:0];
 		shamt = instruction[15:10];
+		enLoad  = 1'b0;
+		enStore = 1'b0;
+		imm_sig = 1'b0;
+		aluControl = op_lsl;
 		end
 	
 	//LDURSW
@@ -86,6 +141,9 @@ module instr_decoder();
 		data_adx = instruction[20:12];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b1;
+		enStore = 1'b0;
+		imm_sig = 1'b0;
 		end
 	
 	//STURW
@@ -93,6 +151,9 @@ module instr_decoder();
 		data_adx = instruction[20:12];
 		rn = instruction[9:5];
 		rd = instruction[4:0];
+		enLoad  = 1'b0;
+		enStore = 1'b1;
+		imm_sig = 1'b0;
 		end
 	
 	//Branch
