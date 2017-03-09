35b9d7a632a8d16d95803bf1e611cfaa683b7fe4
baotantran <baotantran92@gmail.com>
2017-03-09 13:05:00 -0800

    Update SRAM.v

--- Lab4/Source Code/SRAM.v
+++ Lab4/Source Code/SRAM.v
@@ -1,5 +1,5 @@
 module sramf(data, addr, write_data, we, oe, clk);
-	input oe, we, clk;
+	input oe, we, clk; // we write enable(active high), oe out enable (active low)
 	input [10:0] addr;
 
 	output [15:0] data;
@@ -10,13 +10,44 @@ module sramf(data, addr, write_data, we, oe, clk);
 	
 	assign data = oe ?  sramArray[addr] : 16'bZ;
 
-	always @ (posedge clk)
-		begin
+	always @ (posedge clk) begin
 			if (we)
 				sramArray[addr] <= write_data;
-		end
+	end
 		
 	initial begin
-		$readmemb("data.dat", sramArray);
+		sramArray[0] <= 16'd9;
+		sramArray[1] <= 16'd3;
+		sramArray[2] <= 16'd8;
 	end
 endmodule
+
+module sramf_testbench();
+
+	reg oe, we, clk; // we write enable(active high), oe out enable (active low)
+	reg [10:0] addr;
+
+	wire [15:0] data;
+	
+	reg [15:0] write_data;
+	
+	parameter CLOCK_PERIOD=100;
+	initial begin
+	clk <= 0;
+	forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	sramf dux (data, addr, write_data, we, oe, clk);
+	
+	initial begin 
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		
+	$stop();
+	end
+endmodule 
+
+
+		
+		
