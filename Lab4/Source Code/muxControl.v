10066a899ee68f4a0f12d34288fad4b363e7cd62
masnth <masker0801@gmail.com>
2017-03-11 09:49:28 -0800

    change order of input

--- Lab4/Source Code/muxControl.v
+++ Lab4/Source Code/muxControl.v
@@ -17,10 +17,10 @@ module muxControl (reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7,
  
  generate
 	 for (i = 0; i < 32; i = i + 1) begin : eachmux
-		mux32to1 mux (.data({reg0[i],reg1[i], reg2[i], reg3[i], reg4[i], reg5[i], reg6[i], reg7[i], reg8[i], reg9[i], 
-		reg10[i], reg11[i], reg12[i], reg13[i], reg14[i], reg15[i], reg16[i], reg17[i], reg18[i],reg19[i],
-		reg20[i], reg21[i], reg22[i], reg23[i], reg24[i], reg25[i], reg26[i], reg27[i],reg28[i], reg29[i], 
-		reg30[i], reg31[i]}), .q(readData[i]), .sel(sel));
+		mux32to1 mux (.data({reg31[i],reg30[i], reg29[i], reg28[i], reg27[i], reg26[i], reg25[i], reg24[i], reg23[i], reg22[i], 
+		reg21[i], reg20[i], reg19[i], reg18[i], reg17[i], reg16[i], reg15[i], reg14[i], reg13[i],reg12[i],
+		reg11[i], reg10[i], reg9[i], reg8[i], reg7[i], reg6[i], reg5[i], reg4[i],reg3[i], reg2[i], 
+		reg1[i], reg0[i]}), .q(readData[i]), .sel(sel));
 	end
  endgenerate
 endmodule
